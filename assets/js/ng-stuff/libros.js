963fe45bb428e2291e5aa3afdfd2474cfe094ab5
luissfletes <luissfletes@gmail.com>
2015-10-24 01:49:21 -0600

    Create, Upadte, FindAll y Delete Listos (Obras)

--- assets/js/ng-stuff/libros.js
+++ assets/js/ng-stuff/libros.js
@@ -15,19 +15,22 @@ function($scope, $http, ObrasService, GruposService){
 	$scope.tipos = [];
 	$scope.obras = [];
 	$scope.newObra = {};
+	$scope.editObra = {};
 	
 	$scope.addingObra = false;
+	$scope.editingObra = false;
+	$scope.filter = {
+		obras: ''
+	};
 	
 	GruposService.getAll(function(err, body,  stat){
 		if (err) return false;
 		$scope.tipos = body;
-		console.log($scope.tipos);
 	});
 	
 	ObrasService.getAll(function(err, body,  stat){
 		if (err) return false;
 		$scope.obras = body;
-		console.log($scope.obras);
 	});
 	
 	$scope.setNewObra = function () {
@@ -35,11 +38,45 @@ function($scope, $http, ObrasService, GruposService){
 		$scope.addingObra = true;
 	};
 	
+	$scope.setEditObra = function (obra) {
+		$scope.editingObra = true;
+		$scope.editObra = {
+			codigo: obra.codigo,
+			nombre: obra.nombre,
+			ncodigo: '',
+			nnombre: ''
+		};
+	}
+	
 	$scope.createObra = function () {
 		ObrasService.create($scope.newObra, function (err, body, stat) {
-			if (!err) $scope.cancelObras(); //si no hay error, limpiar
-			//falta agregar feedback de exito y error 
+			if (err) return alert(err); //error al crear -> cambiar a otro tipo de feedback
+			updateCollection(1, body, $scope.obras);  //feedback de exito falta
+			$scope.cancelObras();
 		});
+	};
+	
+	$scope.updateObra = function(){
+		ObrasService.update({
+			codigo: $scope.editObra.ncodigo,
+			nombre: $scope.editObra.nnombre
+		}, $scope.editObra.codigo, function (err, body, stat) {
+			if (err) return alert(err); //error al actualizar -> cambiar a otro tipo de feedback
+			/*********
+				CASO : AL ACTUALIZAR Y CAMBIAR EL CODIGO, EL ALGORITMO NO FUNCIONARA
+				ARREGLAR ASAP!
+			********* */
+			updateCollection(2, body, $scope.obras, "codigo");  //feedback de exito falta
+			$scope.cancelObras();
+		});
+	}
+	
+	$scope.deleteObra = function(obra){
+		confirm("¿Esta seguro de querer Borrar la Obra de codigo "+obra.codigo+"? \n[CAMBIAR ESTO]") &&
+		ObrasService.delete(obra.codigo, function (err, body, stat) {
+			if (err) return alert(err); //error al borrar -> cambiar a otro tipo de feedback
+			updateCollection(3, body, $scope.obras, "codigo"); //feedback de exito falta
+		})
 	};
 	
 	$scope.cancelObras = function () {
@@ -47,19 +84,31 @@ function($scope, $http, ObrasService, GruposService){
 		$scope.editingObra = false;
 	};
 	
-	//Real Time Stuff
-	io.socket.on("obras", function (msg) {
-		console.log("Socket", msg);
-		// Let's see what the server has to say...
-//       switch(msg.verb) {
-// 
-//         case 'created':
-//           $scope.obras.push(msg.data); // (add the new order to the DOM)
-//           $scope.$apply();              // (re-render)
-//           break;
-// 
-//         default: return; // ignore any unrecognized messages
-//       }
-	})
+	function updateCollection(opt, obj, collection, field) {
+		//opt - 1-> es agregar 2 -> cambiar 3 -> borrar
+		
+		if (opt === 1){ //si es agregar
+			//push a coll
+			collection.push(obj);
+		}else{
+			// si no -> buscar index en coleccion
+			var idx = -1;
+			for (var i = 0; i < collection.length; i++) {
+				if (obj[field]  == collection[i][field]){
+					idx = i;
+					break;
+				}
+			}
+			if (idx !== -1){ //indice correcto
+				if (opt === 2){ //cambiar
+					collection[idx] = obj;
+				}else{
+					//si no -> es borrar
+					collection.splice(idx, 1);	
+				}
+			}
+		}
+		// $scope.$apply();
+	}
 	
 }]);
--- assets/js/ng-stuff/services.js
+++ assets/js/ng-stuff/services.js
@@ -18,6 +18,24 @@ codesa
 			.error(function(err, stat){
 				callback(err, null, stat);
 			})
+		},
+		update: function (obra, obraId, callback) {
+			$http.post("/obras/"+obraId, obra)
+			.success(function(body, stat){
+				callback(null, body, stat);
+			})
+			.error(function(err, stat){
+				callback(err, null, stat);
+			})	
+		},
+		delete: function(obraId, callback){
+			$http.delete("/obras/"+obraId)
+			.success(function(body, stat){
+				callback(null, body, stat);
+			})
+			.error(function(err, stat){
+				callback(err, null, stat);
+			})
 		}
 	}
 }])
