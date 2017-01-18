6c5c113f7d930c5255b1170dd93c79f0c996cf50
Luis Fletes <luissfletes@gmail.com>
2017-01-17 22:01:53 -0600

    ng animate fix & bcrypt-nodejs

--- api/models/User.js
+++ api/models/User.js
@@ -5,7 +5,7 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 
 module.exports = {
 
@@ -28,7 +28,7 @@ module.exports = {
     },
     beforeCreate: function(user, cb) {
         bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(user.password, salt, function(err, hash) {
+            bcrypt.hash(user.password, salt, null, function(err, hash) {
                 if (err) {
                     console.log(err);
                     cb(err);
